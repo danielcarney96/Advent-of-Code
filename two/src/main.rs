98941618922e91a5acc55b1f6f99c04b633bfae4
danielcarney96 <dancarney@live.co.uk>
2021-12-02 16:38:06 +0000

    Working solution for day2 part2.

--- two/src/main.rs
+++ two/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     part_one();
+    part_two();
 }
 
 pub fn part_one() {
@@ -24,6 +25,38 @@ pub fn part_one() {
             },
             "up" => {
                 depth -= value;
+            },
+            _ => ()
+        }
+    }
+
+    println!("{}", horizontal * depth);
+}
+
+pub fn part_two() {
+    let mut horizontal: i32 = 0;
+    let mut depth: i32 = 0;
+    let mut aim: i32 = 0;
+
+    let lines: Vec<Vec<&str>> = include_str!("../input.txt")
+        .lines()
+        .map(|line| line.split_whitespace().collect())
+        .collect();
+
+    for line in lines {
+        let instruction = line[0];
+        let value: i32 = line[1].parse().unwrap();
+
+        match instruction {
+            "forward" => {
+                horizontal += value;
+                depth += aim * value;
+            },
+            "down" => {
+                aim += value;
+            },
+            "up" => {
+                aim -= value;
             },
             _ => ()
         }
