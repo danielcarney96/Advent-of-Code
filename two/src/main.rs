c80f1170f326950b89b630a61c38a9ca44458d6f
danielcarney96 <dancarney@live.co.uk>
2021-12-02 16:20:22 +0000

    Working solution for day two part one.

---
+++ two/src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+    part_one();
+}
+
+pub fn part_one() {
+    let mut horizontal: i32 = 0;
+    let mut depth: i32 = 0;
+
+    let lines: Vec<Vec<&str>> = include_str!("../input.txt")
+        .lines()
+        .map(|line| line.split_whitespace().collect())
+        .collect();
+
+    for line in lines {
+        let instruction = line[0];
+        let value: i32 = line[1].parse().unwrap();
+
+        match instruction {
+            "forward" => {
+                horizontal += value;
+            },
+            "down" => {
+                depth += value;
+            },
+            "up" => {
+                depth -= value;
+            },
+            _ => panic!()
+        }
+    }
+
+    println!("{}", horizontal * depth);
+}
