cd7484ac655255a441c5a96f2af4bb0ba89b0d12
danielcarney96 <dancarney@live.co.uk>
2021-12-04 20:19:56 +0000

    Working solution for day4 part2.

--- four/src/main.rs
+++ four/src/main.rs
@@ -2,12 +2,13 @@ use std::process;
 
 fn main() {
     part_one();
+    part_two();
 }
 
 type Card = Vec<Vec<usize>>;
 
 // Function to check if a given card has a full row or column of numbers in an array
-fn has_card_won(numbers: &[usize], card: &Card) {
+fn has_card_won(numbers: &[usize], card: &Card) -> Option<usize> {
     for i in 0..5 {
         // Check rows
         if (0..5).all(|n| numbers.contains(&card[i][n])) {
@@ -17,8 +18,7 @@ fn has_card_won(numbers: &[usize], card: &Card) {
                 .filter(|number| !numbers.contains(number))
                 .sum();
 
-            println!("{}", last_number * unmarked);
-            process::exit(1);
+            return Some(last_number * unmarked);
         }
 
         // Check columns
@@ -29,10 +29,11 @@ fn has_card_won(numbers: &[usize], card: &Card) {
                 .filter(|number| !numbers.contains(number))
                 .sum();
 
-            println!("{}", last_number * unmarked);
-            process::exit(1);
+            return Some(last_number * unmarked);
         }
     }
+
+    return None;
 }
 
 fn part_one() {
@@ -65,9 +66,71 @@ fn part_one() {
     // Loop once for each number in the draw
     for i in 0..draw_numbers.len() {
         // For each draw number, check the bingo cards in order
-        for card in &bingo_cards {
-            // Pass the drawn numbers up to the index, used to check new number along with any old ones
-            has_card_won(&draw_numbers[0..i], card);
+
+        let winner = bingo_cards.iter().find_map(|card| has_card_won(&draw_numbers[0..i], card));
+
+        match winner {
+            Some(result) => {
+                println!("{}", result);
+                break;
+            },
+            _ => {},
+        }
+    }
+}
+
+fn part_two() {
+    // Grab the first line and convert into a vector of numbers
+    let draw_numbers: Vec<usize> = include_str!("../input.txt")
+        .lines()
+        .nth(0)
+        .unwrap()
+        .replace(",", " ")
+        .as_str()
+        .split_whitespace()
+        .map(|line| line.trim().parse::<usize>().unwrap())
+        .collect();
+
+    // Grab lines after the first line split into cards, map into a grid structure
+    let mut bingo_cards: Vec<Card> = include_str!("../input.txt")
+        .split("\n\n")
+        .skip(1)
+        .map(|board| {
+            board.lines()
+                .map(|line| {
+                    line.split_whitespace()
+                        .map(|num| num.trim().parse::<usize>().unwrap())
+                        .collect()
+                })
+                .collect()
+        })
+        .collect();
+
+    // Loop once for each number in the draw
+    for i in 0..draw_numbers.len() {
+        // For each draw number, check the bingo cards in order
+
+        // Collect all winners along with their card
+        let winners: Vec<_> = bingo_cards.iter()
+            .filter_map(|card| {
+                has_card_won(&draw_numbers[0..i], &card)
+                    .map(|result| (card.clone(), result))
+            })
+            .collect();
+
+        // Loop each winner
+        for (card, result) in &winners {
+            // If there's more than 1 card left its safe to remove
+            if bingo_cards.len() > 1 {
+                let index = &bingo_cards.iter().position(|i| i == card).unwrap();
+
+                // Remove the card so they can't win more than once
+                bingo_cards.remove(*index);
+            } else {
+                // Last card, return result
+                println!("{}", result);
+                process::exit(1);
+            }
         }
     }
 }
