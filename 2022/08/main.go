ea6aabb32866ae24078aff12484b70091b86f012
danielcarney96 <dancarney@live.co.uk>
2022-12-10 22:38:51 +0000

    Day 8 part 2

--- 2022/08/main.go
+++ 2022/08/main.go
@@ -26,21 +26,22 @@ func main() {
 	}
 
 	fmt.Printf("Part 1: %d\n", calculateVisibleTrees(grid))
+	fmt.Printf("Part 2: %d\n", findHighestScore(grid))
 }
 
 func calculateVisibleTrees(grid [][]int) int {
 	visibleTrees := 0
 	length := len(grid)
 
-	for x := 0; x < length; x++ {
-		for y := 0; y < length; y++ {
+	for y := 0; y < length; y++ {
+		for x := 0; x < length; x++ {
 			// on the boundary
 			if x == 0 || y == 0 || x == length-1 || y == length-1 {
 				visibleTrees++
 				continue
 			}
 
-			// check top
+			// check right
 			visible := true
 			for z := 0; z < y; z++ {
 				if grid[x][z] >= grid[x][y] {
@@ -53,7 +54,7 @@ func calculateVisibleTrees(grid [][]int) int {
 				continue
 			}
 
-			// check bottom
+			// check left
 			visible = true
 			for z := length - 1; z > y; z-- {
 				if grid[x][z] >= grid[x][y] {
@@ -66,7 +67,7 @@ func calculateVisibleTrees(grid [][]int) int {
 				continue
 			}
 
-			// check left
+			// check bottom
 			visible = true
 			for z := 0; z < x; z++ {
 				if grid[z][y] >= grid[x][y] {
@@ -79,7 +80,7 @@ func calculateVisibleTrees(grid [][]int) int {
 				continue
 			}
 
-			// check right
+			// check top
 			visible = true
 			for z := length - 1; z > x; z-- {
 				if grid[z][y] >= grid[x][y] {
@@ -95,4 +96,62 @@ func calculateVisibleTrees(grid [][]int) int {
 	}
 
 	return visibleTrees
+}
+
+func findHighestScore(grid [][]int) int {
+	var score int
+	length := len(grid)
+
+	for y := 0; y < length; y++ {
+		for x := 0; x < length; x++ {
+			// any boundary will sum to 0 so skip it
+			if x == 0 || x == length-1 || y == 0 || y == length-1 {
+				continue
+			}
+
+			// check top
+			visibleTop := 0
+			for z := y - 1; z >= 0; z-- {
+				visibleTop++
+				if grid[z][x] >= grid[y][x] {
+					break
+				}
+			}
+
+			// check bottom
+			visibleBottom := 0
+			for z := y + 1; z < length; z++ {
+				visibleBottom++
+				if grid[z][x] >= grid[y][x] {
+					break
+				}
+			}
+
+			// check left
+			visibleLeft := 0
+			for z := x - 1; z >= 0; z-- {
+				visibleLeft++
+				if grid[y][z] >= grid[y][x] {
+					break
+				}
+			}
+
+			// check right
+			visibleRight := 0
+			for z := x + 1; z < length; z++ {
+				visibleRight++
+				if grid[y][z] >= grid[y][x] {
+					break
+				}
+			}
+
+			currentScore := visibleBottom * visibleTop * visibleLeft * visibleRight
+
+			if currentScore > score {
+				score = currentScore
+			}
+		}
+	}
+
+	return score
 }
