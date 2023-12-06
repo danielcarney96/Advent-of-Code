d7b2555c9cc5e331f319a659a66d00cc475c4bed
danielcarney96 <dancarney@live.co.uk>
2023-12-06 20:12:34 +0000

    wip

--- 2023/05/src/main.rs
+++ 2023/05/src/main.rs
@@ -1,9 +1,11 @@
+use rangetools::Rangetools;
 use std::{collections::HashMap, fs};
 
 fn main() {
     let input: String = fs::read_to_string("./input.txt").unwrap();
 
     part1(&input);
+    part2(&input);
 }
 
 fn part1(input: &String) {
@@ -57,6 +59,78 @@ fn part1(input: &String) {
     }
 
     println!("Part 1: {}", sources.iter().min().unwrap());
+}
+
+fn part2(input: &String) {
+    let (first_line, rest) = input.split_once("\n\n").unwrap();
+
+    let seeds: Vec<i64> = first_line.split("seeds: ").collect::<Vec<&str>>()[1]
+        .split(" ")
+        .map(|seed| seed.parse::<i64>().unwrap())
+        .collect();
+
+    let group_strings = rest.split("\n\n").collect::<Vec<&str>>();
+
+    let mut groups: HashMap<i64, Vec<(i64, i64, i64)>> = HashMap::new();
+
+    for i in 0..group_strings.len() {
+        let (_, group_lines) = group_strings[i].split_once("\n").unwrap();
+
+        group_lines
+            .split("\n")
+            .collect::<Vec<&str>>()
+            .iter()
+            .for_each(|line| {
+                let line_numbers: Vec<i64> = line
+                    .split(" ")
+                    .map(|val: &str| val.parse::<i64>().unwrap())
+                    .collect();
+
+                let new_value = (line_numbers[0], line_numbers[1], line_numbers[2]);
+
+                if let Some(vector) = groups.get_mut(&(i as i64)) {
+                    vector.push(new_value);
+                } else {
+                    let mut new_vector = Vec::new();
+                    new_vector.push(new_value);
+                    groups.insert(i as i64, new_vector);
+                }
+            });
+    }
+
+    let mut sources: Vec<i64> = Vec::new();
+
+    for seed_index in (0..seeds.len()).step_by(2) {
+        let mut intersecting_seeds: Vec<i64> = Vec::new();
+
+        for initial_group_index in 0..groups[&0].len() {
+            let group_to_check = groups[&0].get(initial_group_index).unwrap();
+            let intersecting: Vec<i64> = (seeds[seed_index]
+                ..=seeds[seed_index + 1] + seeds[seed_index])
+                .intersection(group_to_check.1 + 1..=group_to_check.1 + group_to_check.2)
+                .into_iter()
+                .collect();
+
+            if intersecting.len() > 0 {
+                intersecting_seeds = intersecting;
+            }
+        }
+
+        for item in intersecting_seeds.iter() {
+            let mut source: i64 = *item;
+
+            for group_index in 0..groups.len() {
+                source = find_destination_from_source(
+                    source,
+                    groups.get(&(group_index as i64)).unwrap(),
+                );
+            }
+
+            sources.push(source);
+        }
+    }
+
+    println!("Part 2: {}", sources.iter().min().unwrap());
 }
 
 fn find_destination_from_source(source: i64, group: &Vec<(i64, i64, i64)>) -> i64 {
