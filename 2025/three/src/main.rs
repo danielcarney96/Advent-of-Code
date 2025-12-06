3e4c46df35ac19c348e68ce4064f378a5d4812d6
danielcarney96 <dancarney@live.co.uk>
2025-12-06 14:15:01 +0000

    3 pt 2

--- 2025/three/src/main.rs
+++ 2025/three/src/main.rs
@@ -16,7 +16,7 @@ fn main() {
 fn part1(input: &str) {
     let mut sum = 0;
 
-    for (_, line) in input.lines().enumerate() {
+    for line in input.lines() {
         // check line minus last char (as you cannot then get a second char if first comes from the end)
         // largest total number should always start with the biggest digit
         let trimmed_line = &line[..line.len().saturating_sub(1)];
@@ -37,6 +37,38 @@ fn part1(input: &str) {
     println!("{}", sum);
 }
 
+fn part2(input: &str) {
+    let mut sum: i64 = 0;
+
+    const NUMBERS_TO_FIND: usize = 12;
+
+    for line in input.lines() {
+        let mut built_number = String::new();
+        let mut absolute_position = 0;
+
+        for i in (0..NUMBERS_TO_FIND).rev() {
+            let window_start = absolute_position;
+            let window_end = line.len().saturating_sub(i);
+
+            if window_start >= window_end {
+                break;
+            }
+
+            let trimmed_line = &line[window_start..window_end];
+
+            let (found_number, found_number_index) =
+                find_largest_number_in_line_and_position(trimmed_line);
+
+            absolute_position = window_start + found_number_index + 1;
+            built_number = format!("{}{}", built_number, found_number);
+        }
+
+        sum += built_number.parse::<i64>().unwrap();
+    }
+
+    println!("{}", sum);
+}
+
 fn find_largest_number_in_line_and_position(line: &str) -> (u32, usize) {
     const RADIX: u32 = 10;
 
@@ -50,5 +82,3 @@ fn find_largest_number_in_line_and_position(line: &str) -> (u32, usize) {
 
     return (0, 0);
 }
-
-fn part2(input: &str) {}
