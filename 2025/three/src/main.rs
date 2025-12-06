3d825dc0971d257d74cff8c14698c4839af5ddaf
danielcarney96 <dancarney@live.co.uk>
2025-12-06 14:39:55 +0000

    better error handling

--- 2025/three/src/main.rs
+++ 2025/three/src/main.rs
@@ -1,43 +1,40 @@
+use anyhow::Result;
 use std::fs;
 
-fn main() {
-    let input = match fs::read_to_string("./input.txt") {
-        Ok(content) => content,
-        Err(err) => {
-            eprintln!("Error reading file: {}", err);
-            return;
-        }
-    };
+fn main() -> Result<()> {
+    let input = fs::read_to_string("./input.txt")?;
 
-    part1(&input);
-    part2(&input);
+    part1(&input)?;
+    part2(&input)?;
+
+    Ok(())
 }
 
-fn part1(input: &str) {
+fn part1(input: &str) -> Result<()> {
     let mut sum = 0;
 
     for line in input.lines() {
-        let largest_number = find_largest_possible_number_from_string(line, 2);
-
+        let largest_number = find_largest_possible_number_from_string(line, 2)?;
         sum += largest_number;
     }
 
     println!("{}", sum);
+    Ok(())
 }
 
-fn part2(input: &str) {
+fn part2(input: &str) -> Result<()> {
     let mut sum: i64 = 0;
 
     for line in input.lines() {
-        let largest_number = find_largest_possible_number_from_string(line, 12);
-
+        let largest_number = find_largest_possible_number_from_string(line, 12)?;
         sum += largest_number;
     }
 
     println!("{}", sum);
+    Ok(())
 }
 
-fn find_largest_possible_number_from_string(line: &str, numbers_to_find: usize) -> i64 {
+fn find_largest_possible_number_from_string(line: &str, numbers_to_find: usize) -> Result<i64> {
     let mut built_number = String::new();
     let mut absolute_position = 0;
 
@@ -52,25 +49,29 @@ fn find_largest_possible_number_from_string(line: &str, numbers_to_find: usize) 
         let trimmed_line = &line[window_start..window_end];
 
         let (found_number, found_number_index) =
-            find_largest_number_in_line_and_position(trimmed_line);
+            find_largest_number_in_line_and_position(trimmed_line)?;
 
         absolute_position = window_start + found_number_index + 1;
         built_number = format!("{}{}", built_number, found_number);
     }
 
-    return built_number.parse::<i64>().unwrap();
+    Ok(built_number.parse::<i64>()?)
 }
 
-fn find_largest_number_in_line_and_position(line: &str) -> (u32, usize) {
+fn find_largest_number_in_line_and_position(line: &str) -> Result<(u32, usize)> {
     const RADIX: u32 = 10;
 
     for number in (1..=9).rev() {
         for (line_char_index, line_char) in line.chars().enumerate() {
-            if number == line_char.to_digit(RADIX).unwrap() {
-                return (number, line_char_index);
+            if number
+                == line_char
+                    .to_digit(RADIX)
+                    .ok_or_else(|| anyhow::anyhow!("Invalid character: {}", line_char))?
+            {
+                return Ok((number, line_char_index));
             }
         }
     }
 
-    return (0, 0);
+    Ok((0, 0))
 }
